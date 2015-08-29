8f0a397d45072fa99a3f12d8077174fd7ed147f8
Mariano Gappa <mariano@movio.co>
2015-08-29 12:58:07 +1200

    Added test for changing table drawing characters

--- test/TableRendererTest.php
+++ test/TableRendererTest.php
@@ -102,4 +102,37 @@ NOWDOC;
 
         $this->assertEquals($expected, TableRenderer::forArray($test, ["First Name", "Email Address", "Post Address"]));
     }
+
+    public function testRendersProperlyIfCustomTableDrawingOptionsAreSet() {
+        $test = [
+            [
+                "field1" => "Value 1",
+                "field2" => "Value 2",
+                "field3" => "Value 3"
+            ],
+            [
+                "field1" => "Value 4",
+                "field2" => "Value 5 is slightly longer",
+                "field3" => "Value 6"
+            ],
+            [
+                "field1" => "Value 7",
+                "field2" => "Value 8",
+                "field3" => "Value 9 is actually a lot longer than others"
+            ]
+        ];
+
+        $expected = <<<'NOWDOC'
+#****************************************************************************************#
+| First Name | Email Address              | Post Address                                 |
+#****************************************************************************************#
+| Value 1    | Value 2                    | Value 3                                      |
+| Value 4    | Value 5 is slightly longer | Value 6                                      |
+| Value 7    | Value 8                    | Value 9 is actually a lot longer than others |
+#****************************************************************************************#
+
+NOWDOC;
+
+        $this->assertEquals($expected, TableRenderer::forArray($test, ["First Name", "Email Address", "Post Address"], "#", "*", "|"));
+    }
 }
